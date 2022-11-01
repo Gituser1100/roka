2156d12f7233296227c19c336054eb735e94a386
Keyz400 <109141199+Keyz400@users.noreply.github.com>
2022-11-01 14:22:49 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -7,9 +7,9 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("API_ID", default=None, cast=int)
-API_HASH = config("API_HASH", default=None)
-BOT_TOKEN = config("BOT_TOKEN", default=None)
-BOT_UN = config("BOT_UN", default=None)
+API_ID = config("API_ID", default=11321821, cast=int)
+API_HASH = config("API_HASH", default=bbf63cf902c2548c31963ccd88c4c6f7)
+BOT_TOKEN = config("BOT_TOKEN", default=5664270366:AAEpERaV7G45boA7pw3oFnZ9Te83pfWLvDA)
+BOT_UN = config("BOT_UN", default=ret_ddbot)
 
 Drone = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
