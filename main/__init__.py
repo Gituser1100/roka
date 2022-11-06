04063d662bcdd1f94e797591c22824b00e398723
Gituser1100 <114802591+Gituser1100@users.noreply.github.com>
2022-11-06 15:46:46 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -9,7 +9,7 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
 # variables
 API_ID = int("11321821")
 API_HASH = "bbf63cf902c2548c31963ccd88c4c6f7"
-BOT_TOKEN = "5664270366:AAEpERaV7G45boA7pw3oFnZ9Te83pfWLvDA"
-BOT_UN = "ret_ddbot"
+BOT_TOKEN = "5615063323:AAHzWuekv-wIvLHRJo3cOcAhj1daMZxsqEI"
+BOT_UN = "venom100bot"
 
 Drone = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
